b50294942097feeb5cf65b16c08e77442e1e401b
zksailor534 <nate.greene@gmail.com>
2016-06-01 10:15:48 -0400

    Working, kind of -- problems with React 15

--- src/client/modules/inventory/components/lotlist.js
+++ src/client/modules/inventory/components/lotlist.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import Griddle from 'griddle-react';
+import DataGrid from 'react-datagrid';
 
-class LotList extends React.Component {
-  render() {
-    return (
-      <Griddle
-        results={this.props.lots}
-        showFilter={true}
-        columns={[ 'recordId', 'productName' ]}
-      />
-    );
-  }
-}
+const columns = [
+  { name: 'recordId', title: 'Record ID' },
+  { name: 'productName', title: 'Product' }
+];
 
-LotList.propTypes = {
-  lots: React.PropTypes.array
-};
+const LotList = ({lots}) => (
+  <DataGrid
+    idProperty='_id'
+    dataSource={lots}
+    columns={columns}
+    withColumnMenu={false}
+  />
+);
 
 export default LotList;
