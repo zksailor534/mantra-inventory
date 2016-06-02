bb6f25d0bbcdf302ade33a7c20e54eddabc3a4ff
zksailor534 <nate.greene@gmail.com>
2016-06-02 12:23:38 -0400

    Expand navigation to include login & register

--- src/client/modules/core/components/navigation.js
+++ src/client/modules/core/components/navigation.js
@@ -2,22 +2,34 @@ import React from 'react';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import Nav from 'react-bootstrap/lib/Nav';
 import NavItem from 'react-bootstrap/lib/Navitem';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
 
-const Navigation = () => (
-  <Navbar fixedTop>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="/">Mantra Inventory</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} href="/inventory/list">Inventory</NavItem>
-        <NavItem eventKey={2} href="/inventory/new">New Lot</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+class Navigation extends React.Component {
+  render() {
+    return (
+      <Navbar fixedTop fluid>
+        <Navbar.Header>
+          <Navbar.Toggle />
+          <Navbar.Brand>
+            <a href="/">Mantra Inventory</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href='/inventory/list'>Inventory</NavItem>
+            <NavItem eventKey={2} href='/inventory/new'>New Lot</NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavDropdown eventKey={1} title='Account' id='account-dropdown'>
+              <MenuItem eventKey={1.1} href='/login'>Login</MenuItem>
+              <MenuItem eventKey={1.2} href='/register'>Register</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
 
 export default Navigation;
