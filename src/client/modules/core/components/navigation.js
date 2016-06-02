6da93e5ff4a51e8ff2d31da02ebe686ae5598123
zksailor534 <nate.greene@gmail.com>
2016-06-02 11:56:23 -0400

    Add bootstrap with flatly theme - convert navigation to navbar

--- src/client/modules/core/components/navigation.js
+++ src/client/modules/core/components/navigation.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/Navitem';
 
 const Navigation = () => (
-  <div>
-    <b> Navigation: </b>
-    <a href="/">Home</a> |
-    <a href="/inventory/list">Inventory</a> |
-    <a href="/inventory/new">New Lot</a>
-  </div>
+  <Navbar fixedTop>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/">Mantra Inventory</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="/inventory/list">Inventory</NavItem>
+        <NavItem eventKey={2} href="/inventory/new">New Lot</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 export default Navigation;
