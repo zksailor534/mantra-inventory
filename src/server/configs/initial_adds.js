c734ed3ab992fafa98f8e011417839bb6bcc8a41
zksailor534 <nate.greene@gmail.com>
2016-06-23 09:45:02 -0400

    Initial commit with react-bootstrap-table

--- src/server/configs/initial_adds.js
+++ src/server/configs/initial_adds.js
@@ -2,10 +2,24 @@ import { Lots } from 'lib/collections';
 
 export default function () {
   if (!Lots.findOne()) {
-    for (let lc = 1; lc <= 5; lc++) {
+    for (let lc = 1; lc <= 15; lc++) {
       const recordId = `AB-000${lc}`;
       const productName = `Product ${lc}`;
-      Lots.insert({ recordId, productName });
+      const color = `Color ${lc}`;
+      const condition = 'B';
+      const vendor = 'RE-INV';
+      const description = `Description ${lc}`;
+      const price = lc;
+
+      Lots.insert({
+        recordId,
+        productName,
+        color,
+        condition,
+        vendor,
+        description,
+        price
+      });
     }
   }
 }
